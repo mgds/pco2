060281ccf3e0e2f1829d6f92e6931cbb3e668475
John Morton <jmorton@ldeo.columbia.edu>
2019-12-19 10:56:48 -0600

    FAQ pages launched

--- public/scripts/logic/pco2Controllers.js
+++ public/scripts/logic/pco2Controllers.js
@@ -6,17 +6,18 @@ var pco2Controllers = angular.module('pco2Controllers', ['ngSanitize'])
   });
 }]);
 
+pco2Controllers.filter("trust", ['$sce', function($sce) {
+  return function(htmlCode){
+    return $sce.trustAsHtml(htmlCode);
+  };
+}]);
+
 pco2Controllers.run(function($rootScope){
   $rootScope.Utils = {
      keys : Object.keys
   };
 
 });
-pco2Controllers.filter("trust", ['$sce', function($sce) {
-  return function(htmlCode){
-    return $sce.trustAsHtml(htmlCode);
-  };
-}]);
 
 pco2Controllers.controller('indexView',['$scope','$timeout',function($scope,$timeout) {
     $timeout(function(){ $(".centralcontent").hide().fadeIn(500); }, 500);
@@ -25,4 +26,31 @@ pco2Controllers.controller('indexView',['$scope','$timeout',function($scope,$tim
 pco2Controllers.controller('aboutView',['$scope','$timeout','$window',function($scope,$timeout,$window) {
     //$window.scrollTo({ top: 0, behavior: 'smooth' });
     $timeout(function(){ $window.scrollTo({top:0}); }, 200);
+}]);
+
+pco2Controllers.controller('faqsView',['$scope','$timeout','$window','apiConfig',function($scope,$timeout,$window,apiConfig) {
+    //$window.scrollTo({ top: 0, behavior: 'smooth' });
+    $scope.faqs = apiConfig.faqPages;
+    $timeout(function(){ $window.scrollTo({top:0});
+      $('.collapsible').click(function(e){
+        e.preventDefault();
+        var target_element= $(this).attr("data-target");
+        console.log(target_element);
+        $(target_element).toggleClass('collapse1');
+        $(this).find(".glyphicon").toggleClass('glyphicon-menu-down').toggleClass('glyphicon-menu-right');
+        return false;
+      }); 
+    }, 1000);
+}]);
+
+pco2Controllers.controller('faqView',['$scope','$timeout','$window','apiConfig','$routeParams','$location',function($scope,$timeout,$window,apiConfig,$routeParams,$location) {
+    //$window.scrollTo({ top: 0, behavior: 'smooth' });
+    $scope.faqs = apiConfig.faqPages;
+    $scope.faq = $scope.faqs[$routeParams.faqid];
+    if (!$scope.faqs[$routeParams.faqid]) {
+      $location.path("/faqs");
+    }
+    $timeout(function(){
+      $window.scrollTo({top:0});
+    }, 200);
 }]);
